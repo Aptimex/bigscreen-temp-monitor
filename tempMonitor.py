ff19b492a3cf5320ce5839d17493c044290d2866
Aptimex <adammerrill13@gmail.com>
2025-12-27 00:39:00 -0700

    support xTime time-only; kill program on window close

--- tempMonitor.py
+++ tempMonitor.py
@@ -4,11 +4,14 @@ import re
 import time
 import os
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import argparse
+import sys
 
 # Raw string avoids needing to escape backslashes
 LOGFILE = r"C:\Program Files (x86)\Steam\steamapps\common\Bigscreen Beyond Driver\bin\log.txt"
+
+EXIT=False
 
 # This function courtesy of Google AI
 def follow(targetFile):
@@ -24,6 +27,32 @@ def follow(targetFile):
             continue
         yield line
 
+# Parse datetime string which may be either full date+time or just time
+# If just time, assume today's date
+def parseDT(dtString):
+    try:
+        return datetime.strptime(dtString, r"%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        pass
+
+    time = datetime.strptime(dtString, r"%H:%M:%S")
+    today = date.today()
+
+    final_datetime = datetime(
+        year = today.year, 
+        month = today.month, 
+        day = today.day, 
+        hour = time.hour, 
+        minute = time.minute
+    )
+
+    return final_datetime
+
+def on_close(event):
+    global EXIT
+    EXIT = True
+    print("Window Closed. Exiting...")
+    sys.exit(0)
 
 def main(args):
     dtFormmat = r"%Y-%m-%d %H:%M:%S"
@@ -42,16 +71,16 @@ def main(args):
             return
         print(f"Showing last {dataSeconds} seconds of data.")
     elif args.startTime and args.endTime:
-        print(f"Showing data from {datetime.strptime(args.startTime, dtFormmat)} to {datetime.strptime(args.endTime, dtFormmat)}.")
+        print(f"Showing data from {parseDT(args.startTime)} to {parseDT(args.endTime)}.")
     elif args.startTime:
-        print(f"Showing data from {datetime.strptime(args.startTime, dtFormmat)} to present.")
+        print(f"Showing data from {parseDT(args.startTime)} to present.")
     elif args.endTime:
-        print(f"Showing data from start to {datetime.strptime(args.endTime, dtFormmat)}.")
+        print(f"Showing data from start to {parseDT(args.endTime)}.")
     else:
         print("Showing all data.")
     
 
-    print("Opening and parsing log file, please wait...")
+    print("Opening and parsing log file")
     with open(LOGFILE, "r") as lf:
         ti = []
         temps = []
@@ -61,7 +90,7 @@ def main(args):
             # Only parse lines starting with YYYY-MM-DD string
             if re.match(r'^\d{4}-\d{2}-\d{2}', line):
                 time = line.split(",")[0]
-                timeDT = datetime.strptime(time, dtFormmat)
+                timeDT = parseDT(time)
                 ti.append(timeDT)
 
                 temp = line.split(",")[2]
@@ -74,26 +103,34 @@ def main(args):
         plt.title("BSB Temperature over Time")
         animated_plot = plt.plot(ti, temps, 'bo')[0]
 
+        print("File parsed, preparing plot for display...")
+        fig = plt.figure(1)
+        fig.canvas.mpl_connect('close_event', on_close)
+
         if args.endTime:
-            endTimeDT = datetime.strptime(args.endTime, dtFormmat)
+            endTimeDT = parseDT(args.endTime)
             plt.xlim(right = endTimeDT + timedelta(seconds=100))
             if args.startTime:
-                startTimeDT = datetime.strptime(args.startTime, dtFormmat)
+                startTimeDT = parseDT(args.startTime)
                 plt.xlim(left = startTimeDT - timedelta(seconds=100))
             plt.show()
             return
         
+
         # turn interactive mode on to enable real-time updating
         plt.ion() 
 
         if args.startTime:
-            plt.xlim(left = datetime.strptime(args.startTime, dtFormmat) - timedelta(seconds=100))
+            ll = parseDT(args.startTime) - timedelta(seconds=100)
+            plt.xlim(left = ll)
+        #    print(f"Left limit: {ll}")
+        #plt.draw()
 
         # Plot new data points as they are written to the log file
         for line in follow(lf):
             if re.match(r'^\d{4}-\d{2}-\d{2}', line):
                 time = line.split(",")[0]
-                thisTime = datetime.strptime(time, dtFormmat)
+                thisTime = parseDT(time)
                 ti.append(thisTime)
 
                 temp = line.split(",")[2]
@@ -112,7 +149,8 @@ def main(args):
                 plotTemps = temps[cutIndex:]
             
             elif args.startTime:
-                startTimeDT = datetime.strptime(args.startTime, dtFormmat)
+                startTimeDT = parseDT(args.startTime)
+                cutIndex = None
                 for t in plotTi:
                     if t >= startTimeDT:
                         cutIndex = plotTi.index(t)
@@ -139,8 +177,8 @@ if __name__ == "__main__":
     parser.add_argument("--lastHours", "-H", type=int, help="Show this many previous hours of data. Will be added to other --lastX arguments")
     parser.add_argument("--lastMinutes", "-M", type=int, help="Show this many previous minutes of data. Will be added to other --lastX arguments")
     parser.add_argument("--lastSeconds", "-S", type=int, help="Show this many previous seconds of data. Will be added to other --lastX arguments")
-    parser.add_argument("--startTime", "-s", help="Show data starting from this time. Not compatible with --lastX arguments. Format: 'YYYY-MM-DD HH:MM:SS'")
-    parser.add_argument("--endTime", "-e", help="Show data ending at this time. Not compatible with --lastX arguments. Format: 'YYYY-MM-DD HH:MM:SS'")
+    parser.add_argument("--startTime", "-s", help="Show data starting from this time. Not compatible with --lastX arguments. Format: 'YYYY-MM-DD HH:MM:SS'  or 'HH:MM:SS' (assume today's date).")
+    parser.add_argument("--endTime", "-e", help="Show data ending at this time. Not compatible with --lastX arguments. Format: 'YYYY-MM-DD HH:MM:SS' or 'HH:MM:SS' (assume today's date).")
     args = parser.parse_args()
 
     LOGFILE = args.logfile
